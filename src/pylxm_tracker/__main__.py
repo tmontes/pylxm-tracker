86616c2c4fb5f64c8a8a4d3948d1b5a283d744bb
Tiago Montes <self@tmont.es>
2026-04-30 19:01:13 +0100

    CLI now accepts multiple meetup group references.

--- src/pylxm_tracker/__main__.py
+++ src/pylxm_tracker/__main__.py
@@ -7,7 +7,7 @@ from . import db, xtract
 
 
 MEETUP_BASE_URL = 'https://www.meetup.com'
-DEFAULT_GROUP_REF = 'python-lisbon-meetup'
+DEFAULT_GROUP_REFERENCES = ('python-lisbon-meetup', 'python-lisbon')
 
 
 @click.group()
@@ -26,28 +26,25 @@ def main(ctx, db_path):
 
 
 @main.command()
-@click.option(
-    '--meetup-ref',
-    default=DEFAULT_GROUP_REF,
-    show_default=True,
-    help="""
-    The meetup.com group reference (eg: python-lisbon)
-    """
-)
+@click.argument('meetup_references', nargs=-1, default=DEFAULT_GROUP_REFERENCES)
 @click.pass_context
-def collect(ctx, meetup_ref):
+def collect(ctx, meetup_references):
 
-    top_level_url = f'{MEETUP_BASE_URL}/{meetup_ref}'
-    response = httpx.get(top_level_url, follow_redirects=True)
-    html = response.text
+    collected = []
+    for meetup_ref in meetup_references:
+        top_level_url = f'{MEETUP_BASE_URL}/{meetup_ref}'
+        response = httpx.get(top_level_url, follow_redirects=True)
+        html = response.text
 
-    group = xtract.group_info_from_html(html)
-    events = xtract.upcoming_events_from_html(html)
+        group_info = xtract.group_info_from_html(html)
+        group_events = xtract.upcoming_events_from_html(html)
+        collected.append((meetup_ref, group_info, group_events))
 
     now = dt.datetime.now(dt.timezone.utc)
     with db.connection(ctx.obj['db_path']) as dbc:
-        db.insert_group(dbc, meetup_ref, group, now)
-        db.insert_events(dbc, meetup_ref, events, now)
+        for meetup_ref, group_info, group_events in collected:
+            db.insert_group(dbc, meetup_ref, group_info, now)
+            db.insert_events(dbc, meetup_ref, group_events, now)
 
 
 @main.command()
